a802994c1a32524e5ab2c93b370b06fa72ad1997
Dhikshil <mdhikshil@gmail.com>
2025-08-18 11:13:39 +0100

    added archive of articles

--- Blog-Code/computer-science-blog/src/App.jsx
+++ Blog-Code/computer-science-blog/src/App.jsx
@@ -3,6 +3,8 @@ import HomePage from './components/Home.jsx';
 
 import Footer from './components/Footer.jsx';
 
+import SearchPage from './components/Search.jsx';
+
 import './App.css'
 
 const HEADER_ACTIONS = [
@@ -43,6 +45,7 @@ function App() {
       <div className='min-h-screen min-w-100 bg-gradient-to-br from-gray-900 via-blue-950 to-gray-900 text-white p-8'>
         <Header actions={HEADER_ACTIONS}>Computer Science Blog</Header>  
         <HomePage />
+        <SearchPage />
       </div>
       <Footer />
     </>
