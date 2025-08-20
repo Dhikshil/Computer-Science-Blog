2754b05a3b43c5fe38888be7b08fce40d3c3c4fb
Dhikshil <mdhikshil@gmail.com>
2025-08-20 10:25:13 +0100

    adding comment features

--- Blog-Code/computer-science-blog/src/pages/Article.jsx
+++ Blog-Code/computer-science-blog/src/pages/Article.jsx
@@ -1,10 +1,13 @@
 import { useParams } from "react-router-dom"
+import { useRef, useState } from "react";
 
 import ARTICLES from "../../ARTICLES.js";
 import USERS from '../../USERS.js'
 
 export default function ArticlePage() {
     const params = useParams();
+
+    const [newComment, setNewComment] = useState(true);
 
     const article = ARTICLES[Number(params.articleId) - 1];
 
@@ -19,6 +22,43 @@ export default function ArticlePage() {
 
     const ratings = [];
 
+    const inputCommentName = useRef();
+    const inputCommentComment = useRef();
+    const inputCommentRating = useRef();
+
+    function formSubmit(event) {
+        event.preventDefault();
+
+        if (inputCommentName.length < 5) {
+            return;
+        };
+        if (inputCommentComment.length < 30 || inputCommentComment.length > 150) {
+            return;
+        };
+
+        if (inputCommentRating < 0 || inputCommentRating > 5) {
+            return;
+        };
+
+        const newComment = {
+            title: inputCommentName.current.value,
+            id: 6,
+            comment: inputCommentComment.current.value,
+            rating: inputCommentRating.current.value,
+            date: '2-3-4',
+        };
+
+        article.comments.push(newComment);
+
+        setNewComment(true);
+
+            inputCommentName.current.value = "";
+            inputCommentComment.current.value = "";
+            inputCommentRating.current.value = "";
+        
+        setNewComment(false);
+    }
+
     return(
         <main className="max-w-4xl mx-auto mt-8 text-left"> 
             <section className="my-8">
@@ -32,17 +72,71 @@ export default function ArticlePage() {
                 <p className="text-[0.95rem] mt-3 text-gray-200">{article.author} | {article.date}</p>
             </section>
             
-            <p className="font-semibold text-[1.45rem]">COMMENTS</p> 
+            <p className="font-semibold text-[1.45rem]">COMMENTS</p>
             <section className="mt-4">
-                
+                <form
+                    onSubmit={formSubmit}
+                    className="w-4/5 mx-auto rounded-xl p-6 bg-black/40 mb-10 space-y-4 shadow-lg"
+                >
+                    <div className="flex flex-col">
+                        <label htmlFor="title" className="mb-1 text-white font-semibold">
+                            Title
+                        </label>
+                        <input
+                            ref={inputCommentName}
+                            type="text"
+                            id="title"
+                            name="title"
+                            defaultValue=""
+                            className="p-2 rounded-md border border-gray-500 bg-black/20 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <label htmlFor="body" className="mb-1 text-white font-semibold">
+                            Your Opinion
+                        </label>
+                        <textarea
+                            ref={inputCommentComment}
+                            name="body"
+                            rows={5}
+                            defaultValue=""
+                            className="p-2 rounded-md border border-gray-500 bg-black/10 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                        />
+                    </div>
+
+                    <div className="flex flex-col">
+                        <label htmlFor="rating" className="mb-1 text-white font-semibold">
+                            Rating
+                        </label>
+                        <div className="flex justify-between">
+                            <input
+                                ref={inputCommentRating}
+                                type="number"
+                                name="rating"
+                                defaultValue={0}
+                                className="p-2 w-24 rounded-md border border-gray-500 bg-black/10 text-white focus:outline-none focus:ring-2 focus:ring-blue-400"
+                            />
+                            <button
+                                type="submit"
+                                className="px-4 py-2 bg-blue-500 text-white rounded-xl shadow hover:bg-blue-600 transition hover:text-gray-300"
+                            >
+                                Submit
+                            </button>
+                        </div>
+                    </div>
+
+
+                </form>
+
                 {article.comments.map((comment) => {
-                    let starRatings = ratings;
+                    let starRatings = [...ratings];
                     for (let i = 0; i < comment.rating; i++) {
                         starRatings.push(<div key={i} className="mt-2">{starIcon}</div>)
                     }
+                    console.log(article.comments.length)
                     return (
-                        <div className="w-4/5 mx-auto rounded-xl py-3 bg-white/20">
-                            <div className="mx-3 " key={comment.id}>
+                        <div className="w-4/5 mx-auto rounded-xl py-3 bg-white/20" key={0}>
+                            <div className="mx-3">
                                 <div className="flex justify-between">
                                     <p className="text-[1.25rem] font-bold">{comment.title}</p>
                                     <div className="flex">
--- Blog-Code/computer-science-blog/src/pages/Search.jsx
+++ Blog-Code/computer-science-blog/src/pages/Search.jsx
@@ -39,7 +39,7 @@ export default function SearchPage() {
                 <div className="relative w-full">
                     <input 
                         ref={inputArticleName} 
-                        type="text" id="simple-search" 
+                        type="text"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                         placeholder="Search article name..." 
                         required 
